2718790c819016bdac30023d8862492bdb289f3b
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-30 17:49:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,6 +10,22 @@ terraform {
   }
 }
 
+ assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
 
 data "aws_region" "current" {}
 
