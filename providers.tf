52d9a4f3a0a882998f7534dd6e139c5ad047f47b
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-30 17:22:26 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,13 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "techbloc-terraform-data"
-    key    = "openshift-311"
-    region = "eu-west-1"
-  }
-}
+
 
 data "aws_region" "current" {}
 
