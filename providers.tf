7ef7cc695a0c8e4ddf550147560a25bcba36b1ee
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-30 17:47:33 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,10 +4,26 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "techbloc-terraform-data"
-    key    = "openshift-311"
+    bucket = "mybucket"
+    key    = "path/to/my/key"
     region = "eu-west-1"
   }
+}
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::mybucket"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject"],
+      "Resource": "arn:aws:s3:::mybucket/path/to/my/key"
+    }
+  ]
 }
 
 data "aws_region" "current" {}
